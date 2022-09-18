080fdc9649a37180423941ee4de969ee17a52fab
ujeon jang <wj9304@gmail.com>
2022-09-18 19:13:14 +0900

    Initial Commit

---
+++ OnlineLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnlineLearning
+//
+//  Created by 우전 on 2022/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnlineLearning/OnlineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineLearningApp.swift
+//  OnlineLearning
+//
+//  Created by 우전 on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct OnlineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
