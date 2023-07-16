c561e56bf33ee798cf5e923b929104e45add2d70
ujeon jang <wj9304@gmail.com>
2023-07-16 13:23:41 +0900

    feat: 워크스루 페이지 추가

---
+++ OnlineLearning/Extensions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  OnlineLearning
+//
+//  Created by 우전 on 2022/10/03.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let blue1 = Color.init(UIColor(red: 0.24, green: 0.36, blue: 1.0, alpha: 1.0))
+  static let blue2 = Color.init(UIColor(red: 0.918, green: 0.918, blue: 1, alpha: 1))
+  static let gray1 = Color.init(UIColor(red: 0.52, green: 0.59, blue: 0.59, alpha: 1))
+}
--- OnlineLearning/OnlineLearningApp.swift
+++ OnlineLearning/OnlineLearningApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct OnlineLearningApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      WalkThroughView()
     }
+  }
 }
